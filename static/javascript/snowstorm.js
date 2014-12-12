07d38aa09c9759b201f9a5b8d00725501a880c24
adamus1red <adamus1red@users.noreply.github.com>
2014-12-12 02:18:37 +0000

    Change snow colour

--- static/javascript/snowstorm.js
+++ static/javascript/snowstorm.js
@@ -23,7 +23,7 @@ var snowStorm = (function(window, document) {
   this.useGPU = true;             // Enable transform-based hardware acceleration, reduce CPU load.
   this.flakeBottom = null;        // Integer for Y axis snow limit, 0 or null for "full-screen" snow effect
   this.followMouse = true;        // Snow movement can respond to the user's mouse
-  this.snowColor = '#fff';        // Don't eat (or use?) yellow snow.
+  this.snowColor = '#EFECE5';        // Don't eat (or use?) yellow snow.
   this.snowCharacter = '&bull;';  // &bull; = bullet, &middot; is square on some systems etc.
   this.snowStick = true;          // Whether or not snow should "stick" at the bottom. When off, will never collect.
   this.targetElement = null;      // element which snow will be appended to (null = document.body) - can be an element ID eg. 'myDiv', or a DOM node reference
